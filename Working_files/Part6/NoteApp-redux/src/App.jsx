db0088c332e20b53bcd2cec216a3b6a6260136ba
ffernnando <ffernnando192@gmail.com>
2025-08-19 17:54:54 +0200

    More redux, stores, providers  and components

--- Working_files/Part6/NoteApp-redux/src/App.jsx
+++ Working_files/Part6/NoteApp-redux/src/App.jsx
@@ -1,43 +1,21 @@
-import { createStore } from 'redux'
-
-const noteReducer = (state = [], action) => {
-  if (action.type === 'NEW_NOTE') {
-    state.push(action.payload)
-    return state
-  }
-  return state
-}
-
-const store = createStore(noteReducer)
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
+import React from 'react'
+import { toggleImportanceOf } from './reducers/noteReducer'
+import { useSelector, useDispatch } from 'react-redux'
+import NewNote from './components/newNote'
+import Notes from './components/Notes'
 
 const App = () => {
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state)
+
+  const toggleImportance = (id) => {
+    dispatch(toggleImportanceOf(id))
+  }
+
   return (
     <div>
-      <ul>
-        {store.getState().map(note =>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
+      <NewNote />
+      <Notes />
     </div>
   )
 }
--- Working_files/Part6/NoteApp-redux/src/main.jsx
+++ Working_files/Part6/NoteApp-redux/src/main.jsx
@@ -1,8 +1,15 @@
-// import { createRoot } from 'react-dom/client'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import noteReducer from './reducers/noteReducer'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
 
+const store = createStore(noteReducer)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
