16eccaa0c9307c989d23444b8144be6641c5734b
ffernnando <ffernnando192@gmail.com>
2025-08-23 11:30:08 +0200

    Solve exercises 7.4.-7.8.

---
+++ Submissions/country-hook/src/hooks/index.js
@@ -0,0 +1,6 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const useCountry = (country) => {
+
+}
--- Submissions/routed-anecdotes/components/CreateNew.jsx
+++ Submissions/routed-anecdotes/components/CreateNew.jsx
@@ -1,22 +1,33 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { useField } from "../src/hooks"
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
+  /* const [content, setContent] = useState('')
   const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const [info, setInfo] = useState('') */
 
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      atuhro: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
+  }
+
+  const resetInput = () => {
+    content.resetValue()
+    author.resetValue()
+    info.resetValue()
   }
 
   return (
@@ -25,17 +36,18 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
+        <button type="button" onClick={resetInput}>reset</button>
       </form>
     </div>
   )
