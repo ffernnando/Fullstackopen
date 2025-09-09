abdafcda5bfba5c2718b7e27c31fb29bbaa536ab
ffernnando <ffernnando192@gmail.com>
2025-09-09 18:35:08 +0200

    The horrors of any and type assertion - Eslint

--- Working_files/Part9/part9a-first-steps/calculator.ts
+++ Working_files/Part9/part9a-first-steps/calculator.ts
@@ -1,6 +1,6 @@
-type Operation = 'multiply' | 'add' | 'divide';
+export type Operation = 'multiply' | 'add' | 'divide';
 
-const calculator = (a: number, b: number, op: Operation): number => {
+export const calculator = (a: number, b: number, op: Operation): number => {
   switch(op) {
     case 'multiply':
       return a * b;
@@ -23,4 +23,3 @@ try {
   }
   console.log(errorMessage)
 }
-
