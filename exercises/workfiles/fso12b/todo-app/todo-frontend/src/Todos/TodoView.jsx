050c77ef4f1b9d35e4639d2783029afecdc13f16
ffernnando <ffernnando192@gmail.com>
2026-02-18 10:02:01 +0100

    Exercise 12.13.

--- exercises/workfiles/fso12b/todo-app/todo-frontend/src/Todos/TodoView.jsx
+++ exercises/workfiles/fso12b/todo-app/todo-frontend/src/Todos/TodoView.jsx
@@ -4,11 +4,15 @@ import axios from '../util/apiClient'
 import List from './List'
 import Form from './Form'
 
+
+//const todo_url = 'http://localhost:3000';
+const todo_url = import.meta.env.VITE_BACKEND_URL;
+
 const TodoView = () => {
   const [todos, setTodos] = useState([])
 
   const refreshTodos = async () => {
-    const { data } = await axios.get('/todos')
+    const { data } = await axios.get(`${todo_url}/todos`)
     setTodos(data)
   }
 
@@ -17,17 +21,17 @@ const TodoView = () => {
   }, [])
 
   const createTodo = async (todo) => {
-    const { data } = await axios.post('/todos', todo)
+    const { data } = await axios.post(`${todo_url}/todos`, todo)
     setTodos([...todos, data])
   }
 
   const deleteTodo = async (todo) => {
-    await axios.delete(`/todos/${todo._id}`)
+    await axios.delete(`${todo_url}/todos/${todo._id}`)
     refreshTodos()
   }
 
   const completeTodo = async (todo) => {
-    await axios.put(`/todos/${todo._id}`, {
+    await axios.put(`${todo_url}/todos/${todo._id}`, {
       text: todo.text,
       done: true
     })
